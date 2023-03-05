fca53940e6dcfa0ea440416d22dd861590f791f0
Artem Krashevskyi <aratar1992@gmail.com>
2023-03-05 23:30:47 +0200

    updated js

--- script.js
+++ script.js
@@ -2,25 +2,36 @@ const loginForm = document.forms.login;
 const nameInput = loginForm.elements.name;
 const greeting = document.getElementById("greeting");
 const todo = document.forms.todo;
+const counter = document.getElementById("counter");
+const search = todo.elements.search;
 const addBtn = document.getElementById("add-btn");
 const itemsDiv = document.getElementById("item-list");
 const allBtn = document.getElementById("all-btn");
 const activeBtn = document.getElementById("active-btn");
 const doneBtn = document.getElementById("done-btn");
 
-const itemList = [];
 let userName;
+const userStorage = localStorage.getItem('user');
+
+if (userStorage) {
+    userName = userStorage;
+    login();
+}
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     userName = nameInput.value;
-    loginForm.hidden = true;
-    greeting.hidden = false;
-    todo.hidden = false;
-    greeting.textContent = `You are welcome, ${userName}!`;
+    localStorage.setItem('user', userName);
+    login();
 })
 
-itemList.forEach(item => addItem(item));
+const listStorage = localStorage.getItem('list');
+const itemList = listStorage ? JSON.parse(listStorage) : [];
+
+if (itemList.length) {
+    itemList.forEach((item, idx) => addItem(item, idx));
+    updateCounter();
+} 
 
 addBtn.addEventListener('click', () => {
     const addInput = document.createElement('input');
@@ -29,56 +40,61 @@ addBtn.addEventListener('click', () => {
     addInput.placeholder = 'Add new item';
 
     addInput.addEventListener('change', () => {
-        const item = { 
-            name: addInput.value, 
-            done: false
+        if (itemList.some(item => item.name === addInput.value)) {
+            alert("This name already exists!");
+        } else {
+            const item = { 
+                name: addInput.value, 
+                done: false
+            }
+    
+            itemList.push(item);
+            const idx = itemList.length - 1;
+            addItem(item, idx);
+            localStorage.setItem('list', JSON.stringify(itemList));
+            updateCounter();
+            addInput.replaceWith(addBtn);
         }
-        itemList.push(item);
-        addItem(item);
+    });
+
+    addInput.addEventListener('blur', () => {
         addInput.replaceWith(addBtn);
-    })
+    });
 
     addBtn.replaceWith(addInput);
 })
 
+search.addEventListener('input', filterItems);
+
 allBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const allList = itemsDiv.children;
-    itemsDiv.append(...allList);
-    // itemList.forEach(item => addItem(item));
+    itemsDiv.innerHTML = '';
+    itemList.forEach((item, idx) => addItem(item, idx));
+    if (search.value) filterItems();
 });
 
 activeBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const allList = Array.from(itemsDiv.children);
-    const activeList = allList.filter(item => {
-       return !item.querySelector('input').checked
-    });
-    console.dir(activeList);
-    itemsDiv.append(...activeList);
-    // itemList.forEach(item => () => {
-    //     if (!item.done) {
-    //         addItem(item);
-    //     }
-    // }) 
+    itemsDiv.innerHTML = '';
+    itemList.forEach((item, idx) => item.done ? false : addItem(item, idx));
+    if (search.value) filterItems();
 });
 
 doneBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const allList = Array.from(itemsDiv.children);
-    const doneList = allList.filter(item => {
-       return item.querySelector('input').checked
-    });
-    console.dir(doneList);
-    itemsDiv.append(...doneList);
-    // itemList.forEach(item => () => {
-    //     if (item.done) {
-    //         addItem(item);
-    //     }
-    // }) 
+    itemsDiv.innerHTML = '';
+    itemList.forEach((item, idx) => item.done ? addItem(item, idx) : false);
+    if (search.value) filterItems();
 });
 
-function addItem (item) {
+function login () {
+    loginForm.hidden = true;
+    greeting.hidden = false;
+    todo.hidden = false;
+    greeting.textContent = `You are welcome, ${userName}!`;
+}
+ 
+function addItem (item, idx) {
     const {name, done} = item;
     const listItem = document.createElement('div');
     const checkbox = document.createElement('input');
@@ -93,35 +109,64 @@ function addItem (item) {
     const delBtn = document.createElement('button');
     delBtn.textContent = 'Delete';
     delBtn.addEventListener('click', deleteItem);
-    listItem.append(checkbox, label, editBtn, delBtn);
+    const hiddenIdx = document.createElement('span');
+    hiddenIdx.textContent = idx;
+    hiddenIdx.hidden = true;
+    listItem.append(checkbox, label, editBtn, delBtn, hiddenIdx);
     itemsDiv.append(listItem);
 }
 
+function filterItems () {
+    for (const item of itemsDiv.children) {
+        const name = item.querySelector('label').textContent;
+        item.hidden = name.includes(search.value) ? false : true;
+    }
+}
+
 function switchDone (e) {
-    const name = e.target.nextElementSibling.textContent;
-    idx = itemList.findIndex(item => item.name === name);
+    const item = e.target.parentElement;
+    const idx = item.querySelector('span').textContent;
     itemList[idx].done = !itemList[idx].done;
+    localStorage.setItem('list', JSON.stringify(itemList));
+    updateCounter();
 }
 
 function editItem (e) {
     const item = e.target.parentElement;
     const label = item.querySelector('label');
     const name = label.textContent;
+    const idx = item.querySelector('span').textContent;
     const editInput = document.createElement('input');
     editInput.value = name;
+
     editInput.addEventListener('change', () => {
-        label.textContent = editInput.value;
-        editInput.replaceWith(item);
-        idx = itemList.findIndex(item => item.name === name);
-        itemList[idx].name = editInput.value;
+        if (itemList.some(item => item.name === editInput.value)) {
+            alert("This name already exists!");
+        } else {
+            itemList[idx].name = editInput.value;
+            localStorage.setItem('list', JSON.stringify(itemList));
+            label.textContent = editInput.value;
+        }
     });
+    
+    editInput.addEventListener('blur', () => {
+        editInput.replaceWith(item);
+    });
+
     item.replaceWith(editInput);
 }
 
 function deleteItem (e) {
     const item = e.target.parentElement;
-    const name = item.querySelector('label').textContent;
-    item.remove();
-    idx = itemList.findIndex(item => item.name === name);
+    const idx = item.querySelector('span').textContent;
     itemList.splice(idx, 1);
+    localStorage.setItem('list', JSON.stringify(itemList));
+    item.remove();
+    updateCounter();
+}
+
+function updateCounter () {
+    const total = itemList.length;
+    const done = itemList.reduce((acc, item) => acc = item.done ? ++acc : acc, 0);
+    counter.textContent = `${done} from ${total} done`;
 }
